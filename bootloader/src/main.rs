775bccf222564f3d1893b5985d3645424df7ad75
Satoshi Kojima <skoji@skoji.jp>
2021-03-27 17:38:05 +0900

    just load kernel to memory

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -7,8 +7,11 @@ extern crate alloc;
 extern crate rlibc;
 use alloc::vec::Vec;
 use core::fmt::Write;
-use uefi::proto::media::file::{File, FileAttribute, FileMode, FileType::Regular};
 use uefi::{prelude::*, proto::media::fs::SimpleFileSystem};
+use uefi::{
+    proto::media::file::{File, FileAttribute, FileInfo, FileMode, FileType::Regular},
+    table::boot::{AllocateType, MemoryType},
+};
 
 #[entry]
 fn efi_main(_handle: Handle, st: SystemTable<Boot>) -> Status {
@@ -27,38 +30,37 @@ fn efi_main(_handle: Handle, st: SystemTable<Boot>) -> Status {
         writeln!(st.stdout(), "no simple filesystem protocol").unwrap();
         panic!("no sfs");
     };
-    let mut directory = sfs.open_volume().unwrap().unwrap();
+    let mut root = sfs.open_volume().unwrap().unwrap();
 
-    let memmap_file = directory
-        .open("memmap", FileMode::CreateReadWrite, FileAttribute::empty())
+    let kernel_file = root
+        .open("rmikan-kernel", FileMode::Read, FileAttribute::READ_ONLY)
         .unwrap()
         .unwrap();
-    let memmap_file = memmap_file.into_type().unwrap().unwrap();
-    if let Regular(mut memmap_file) = memmap_file {
-        let mmap_buf = &mut [0; 4096 * 4];
-        let (_, memmap_iter) = bt.memory_map(mmap_buf).unwrap().unwrap();
-        memmap_file
-            .write("Index, Type, PhysicalStart, NumberOfPages, Attribute\n".as_bytes())
+    let kernel_file = kernel_file.into_type().unwrap().unwrap();
+    if let Regular(mut kernel_file) = kernel_file {
+        const BUF_SIZE: usize = 4000;
+        let buf = &mut [0u8; BUF_SIZE];
+        let info: &mut FileInfo = kernel_file.get_info(buf).unwrap().unwrap();
+        let kernel_file_size = info.file_size();
+        const KERNEL_BASE_ADDRESS: usize = 0x100000;
+        let page_pointer = bt
+            .allocate_pages(
+                AllocateType::Address(KERNEL_BASE_ADDRESS),
+                MemoryType::LOADER_DATA,
+                (kernel_file_size as usize + 0xfff) / 0x1000,
+            )
             .unwrap()
             .unwrap();
-        for (i, m) in memmap_iter.enumerate() {
-            memmap_file
-                .write(
-                    format!(
-                        "{}, {:?}, {}, {}, {:?}\n",
-                        i, m.ty, m.phys_start, m.page_count, m.att
-                    )
-                    .as_bytes(),
-                )
-                .unwrap()
-                .unwrap();
-        }
-        memmap_file.close();
+        let base_pointer = page_pointer as *mut u8;
+        let page_buf =
+            unsafe { core::slice::from_raw_parts_mut(base_pointer, kernel_file_size as usize) };
+        kernel_file.read(page_buf).unwrap().unwrap();
+        kernel_file.close();
     };
 
     let mut buffer = Vec::with_capacity(128);
     loop {
-        let file_info = match directory.read_entry(&mut buffer) {
+        let file_info = match root.read_entry(&mut buffer) {
             Ok(completion) => {
                 if let Some(info) = completion.unwrap() {
                     info
@@ -81,7 +83,7 @@ fn efi_main(_handle: Handle, st: SystemTable<Boot>) -> Status {
         )
         .unwrap();
     }
-    directory.reset_entry_readout().unwrap().unwrap();
+    root.reset_entry_readout().unwrap().unwrap();
 
     loop {}
 }
