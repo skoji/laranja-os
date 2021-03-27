9e797353ccf83777cd814e3b07575dd051c865ff
Satoshi Kojima <skoji@skoji.jp>
2021-03-27 17:38:17 +0900

    update make-image

--- kernel/README.md
+++ kernel/README.md
@@ -1,5 +1,5 @@
 ## build
 
 ```
-cargo +nightly build -Zbuild-std=core,alloc --target x86_64-unknown-none-linuxkernel --release
+cargo +nightly build -Zbuild-std=core,alloc --target x86_64-unknown-linux-gnu --release
 ```
--- make-image.sh
+++ make-image.sh
@@ -9,4 +9,5 @@ rm -rf $MOUNT_POINT
 hdiutil attach -mountpoint $MOUNT_POINT $IMG_NAME
 mkdir -p $MOUNT_POINT/EFI/BOOT
 cp ./bootloader/target/x86_64-unknown-uefi/release/rust-uefi-mikan.efi $MOUNT_POINT/EFI/BOOT/BOOTX64.EFI
+cp .//kernel/target/x86_64-unknown-linux-gnu/release/rmikan-kernel $MOUNT_POINT/rmikan-kernel
 hdiutil detach $MOUNT_POINT
