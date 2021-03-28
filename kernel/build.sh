53b6cce6f5443990c5929978f1b2bc306860688b
Satoshi Kojima <skoji@skoji.jp>
2021-03-28 18:19:42 +0900

    WIP; not working yet.

--- kernel/build.sh
+++ kernel/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd `dirname $0`
-cargo +nightly build -Zbuild-std=core,alloc --target ./x86_64-unkown-none-mikankernel.json --release
+cargo +nightly build -Zbuild-std=core,alloc --target ./x86_64-unknown-none-mikankernel.json --release --verbose
