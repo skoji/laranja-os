f5b1d4fe781880bd2a1afa834188d029c4aaad09
Satoshi Kojima <skoji@skoji.jp>
2021-04-18 19:26:27 +0900

    use newly built OVMFs

--- qemu-run.sh
+++ qemu-run.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-qemu-system-x86_64 -monitor stdio -bios OVMF.fd -drive file=LARANJA.img,format=raw
+qemu-system-x86_64 \
+    -monitor stdio \
+    -drive if=pflash,format=raw,readonly,file=OVMF_CODE.fd \
+    -drive if=pflash,format=raw,file=OVMF_VARS.fd \
+    -drive if=ide,index=0,media=disk,format=raw,file=LARANJA.img
