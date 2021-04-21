2ec85ff28969efd9751c60239554c01777c994c8
Satoshi Kojima <skoji@skoji.jp>
2021-04-21 18:58:38 +0900

    add (totally not working) mouse cursor

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -9,8 +9,37 @@ use laranja_kernel::console::Console;
 use laranja_kernel::graphics::{FrameBuffer, Graphics, ModeInfo, PixelColor};
 use laranja_kernel::{print, println};
 
-static BG_COLOR: PixelColor = PixelColor(32, 32, 32);
+static BG_COLOR: PixelColor = PixelColor(0, 80, 80);
 static FG_COLOR: PixelColor = PixelColor(255, 128, 0);
+
+const MOUSE_CURSOR_HEIGHT: usize = 24;
+
+const MOUSE_CURSOR_SHAPE: [&str; MOUSE_CURSOR_HEIGHT] = [
+    "@              ",
+    "@@             ",
+    "@.@            ",
+    "@..@           ",
+    "@...@          ",
+    "@....@         ",
+    "@.....@        ",
+    "@......@       ",
+    "@.......@      ",
+    "@........@     ",
+    "@.........@    ",
+    "@..........@   ",
+    "@...........@  ",
+    "@............@ ",
+    "@......@@@@@@@@",
+    "@......@       ",
+    "@....@@.@      ",
+    "@...@ @.@      ",
+    "@..@   @.@     ",
+    "@.@    @.@     ",
+    "@@      @.@    ",
+    "@       @.@    ",
+    "         @.@   ",
+    "         @@@   ",
+];
 
 #[no_mangle]
 extern "C" fn kernel_main(fb: *mut FrameBuffer, mi: *mut ModeInfo) {
@@ -40,6 +69,22 @@ extern "C" fn kernel_main(fb: *mut FrameBuffer, mi: *mut ModeInfo) {
     writeln!(writer, "1 + 2 = {}", 1 + 2).unwrap();
     println!("Hello Laranja OS !");
     println!("Resolution {:?}", graphics.resolution());
+
+    // draw mouse cursor which will never move
+    for (dy, l) in MOUSE_CURSOR_SHAPE.iter().enumerate() {
+        for (dx, c) in l.chars().enumerate() {
+            match c {
+                '@' => {
+                    graphics.write_pixel(200 + dx, 100 + dy, &PixelColor(0, 0, 0));
+                }
+                '.' => {
+                    graphics.write_pixel(200 + dx, 100 + dy, &PixelColor(255, 255, 255));
+                }
+                _ => {}
+            }
+        }
+    }
+
     unsafe {
         loop {
             asm!("hlt");
