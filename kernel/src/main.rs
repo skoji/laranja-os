2e5ba2c79f8a90662c43553128cd9358df8cdb1a
Satoshi Kojima <skoji@skoji.jp>
2021-04-30 12:14:21 +0900

    refactor kernel main

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,7 +3,6 @@
 #![feature(asm)]
 #![feature(lang_items)]
 
-use core::fmt::Write;
 use core::panic::PanicInfo;
 use laranja_kernel::console::Console;
 use laranja_kernel::graphics::{FrameBuffer, Graphics, ModeInfo, PixelColor};
@@ -41,36 +40,15 @@ const MOUSE_CURSOR_SHAPE: [&str; MOUSE_CURSOR_HEIGHT] = [
     "         @@@   ",
 ];
 
-#[no_mangle]
-extern "C" fn kernel_main(fb: *mut FrameBuffer, mi: *mut ModeInfo) {
-    // initialize Graphics and Console
+fn initialize(fb: *mut FrameBuffer, mi: *mut ModeInfo) {
     unsafe { Graphics::initialize_instance(fb, mi) }
     Console::initialize(&FG_COLOR, &BG_COLOR);
+    Graphics::instance().clear(&BG_COLOR);
+}
 
-    let graphics = Graphics::instance();
-
-    graphics.clear(&BG_COLOR);
-    let (width, _) = graphics.resolution();
-    let mut x = width / 3;
-    let mut y = 100;
-    for i in 0x20u8..0x7Fu8 {
-        graphics.write_ascii(x, y, i.into(), &PixelColor(0, 255, 0));
-        x += 8;
-        if x > width / 3 * 2 {
-            x = width / 3;
-            y += 32;
-        }
-    }
-    x = width / 3;
-    y += 32;
-    graphics.write_string(x, y, "Hello, Laranja/Mikan OS", &PixelColor(255, 0, 255));
-
-    let mut writer = graphics.text_writer(width / 3, y + 32, &PixelColor(255, 255, 0));
-    writeln!(writer, "1 + 2 = {}", 1 + 2).unwrap();
-    println!("Hello Laranja OS !");
-    println!("Resolution {:?}", graphics.resolution());
-
+fn draw_mouse_cursor() {
     // draw mouse cursor which will never move
+    let graphics = Graphics::instance();
     for (dy, l) in MOUSE_CURSOR_SHAPE.iter().enumerate() {
         for (dx, c) in l.chars().enumerate() {
             let x = 200 + dx;
@@ -86,6 +64,17 @@ extern "C" fn kernel_main(fb: *mut FrameBuffer, mi: *mut ModeInfo) {
             }
         }
     }
+}
+fn welcome_message() {
+    println!("Hello Laranja OS !");
+    println!("Resolution {:?}", Graphics::instance().resolution());
+}
+
+#[no_mangle]
+extern "C" fn kernel_main(fb: *mut FrameBuffer, mi: *mut ModeInfo) {
+    initialize(fb, mi);
+    draw_mouse_cursor();
+    welcome_message();
 
     unsafe {
         loop {
