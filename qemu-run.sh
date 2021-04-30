af9837e869873279331390430b767f282af61e64
Satoshi Kojima <skoji@skoji.jp>
2021-04-30 17:54:57 +0900

    add usb device to qemu option

--- qemu-run.sh
+++ qemu-run.sh
@@ -5,4 +5,6 @@ qemu-system-x86_64 \
     -monitor stdio \
     -drive if=pflash,format=raw,readonly,file=OVMF_CODE.fd \
     -drive if=pflash,format=raw,file=OVMF_VARS.fd \
-    -drive if=ide,index=0,media=disk,format=raw,file=LARANJA.img
+    -drive if=ide,index=0,media=disk,format=raw,file=LARANJA.img \
+    -device nec-usb-xhci,id=xhci \
+    -device usb-mouse -device usb-kbd
