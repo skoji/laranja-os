43ba7664ca3d1302bfe4fa85a2e26b564a577106
Satoshi Kojima <skoji@skoji.jp>
2021-05-08 17:19:12 +0900

    implementing xHCI CapabilityRegisters struct

--- kernel/src/usb.rs
+++ kernel/src/usb.rs
@@ -5,8 +5,8 @@ pub struct CapabilityRegisters {
     cap_length: u8,
     reserved: u8,
     hci_version: u16,
-    hcs_params1: u32,
-    hcs_params2: u32,
+    hcs_params1: HscParam1,
+    hcs_params2: HscParam2,
     hcs_params3: u32,
     hcc_params1: u32,
     db_off: u32,
@@ -18,7 +18,7 @@ impl core::fmt::Display for CapabilityRegisters {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         write!(
             f,
-            "cap_length: {}, hci_version: 0x{:02x}, hcs_params1: 0x{:08x}, hcs_params2: 0x{:08x}, hcs_params3: 0x{:08x}, hcc_params1: 0x{:08x}, db_off: 0x{:08x}, rts_off: 0x{:08x}, hcc_params2: 0x{:08x}",
+            "cap_length: {}, hci_version: 0x{:02x}, hcs_params1: {}, hcs_params2: {}, hcs_params3: 0x{:08x}, hcc_params1: 0x{:08x}, db_off: 0x{:08x}, rts_off: 0x{:08x}, hcc_params2: 0x{:08x}",
             self.cap_length,
             self.hci_version,
             self.hcs_params1,
@@ -29,8 +29,64 @@ impl core::fmt::Display for CapabilityRegisters {
             self.rts_off,
             self.hcc_params2
         )
-        .unwrap();
-        Ok(())
+    }
+}
+
+#[repr(C, packed(4))]
+pub struct HscParam1 {
+    data: u32,
+}
+
+impl HscParam1 {
+    pub fn new(data: u32) -> Self {
+        HscParam1 { data }
+    }
+    pub fn max_device_slots(&self) -> u8 {
+        (self.data & 0xff) as u8
+    }
+
+    pub fn max_ports(&self) -> u8 {
+        (self.data >> 24 & 0xff) as u8
+    }
+}
+
+impl core::fmt::Display for HscParam1 {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(
+            f,
+            "0x{:08x} (slots: {} ports: {})",
+            self.data,
+            self.max_device_slots(),
+            self.max_ports()
+        )
+    }
+}
+
+#[repr(C, packed(4))]
+pub struct HscParam2 {
+    data: u32,
+}
+
+impl HscParam2 {
+    pub fn new(data: u32) -> Self {
+        HscParam2 { data }
+    }
+
+    pub fn max_scratchpad_buf(&self) -> usize {
+        let hi = (self.data >> 21 & 0b11111) as usize;
+        let lo = (self.data >> 27 & 0b11111) as usize;
+        (hi << 5) | lo
+    }
+}
+
+impl core::fmt::Display for HscParam2 {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(
+            f,
+            "0x{:08x} (max_scratchpad_buf: {})",
+            self.data,
+            self.max_scratchpad_buf()
+        )
     }
 }
 
