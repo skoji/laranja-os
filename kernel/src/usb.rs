0d39cd631a4606e752f36c834fcddfa87efe3e42
Satoshi Kojima <skoji@skoji.jp>
2021-05-10 19:34:40 +0900

    refactor; introduce usb::registers module

--- kernel/src/usb.rs
+++ kernel/src/usb.rs
@@ -1,132 +1,7 @@
 use crate::debug;
 
-#[repr(C, packed(4))]
-struct CapabilityRegisters {
-    cap_length: u8,
-    reserved: u8,
-    hci_version: u16,
-    hcs_params1: HscParam1,
-    hcs_params2: HscParam2,
-    hcs_params3: u32,
-    hcc_params1: HccParams1,
-    db_off: u32,
-    rts_off: u32,
-    hcc_params2: u32,
-}
-
-impl core::fmt::Display for CapabilityRegisters {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(
-            f,
-            "cap_length: {}, hci_version: 0x{:02x}, hcs_params1: {}, hcs_params2: {}, hcs_params3: 0x{:08x}, hcc_params1: {}, db_off: 0x{:08x}, rts_off: 0x{:08x}, hcc_params2: 0x{:08x}",
-            self.cap_length,
-            self.hci_version,
-            self.hcs_params1,
-            self.hcs_params2,
-            self.hcs_params3,
-            self.hcc_params1,
-            self.db_off & 0xffff_fffc,
-            self.rts_off & 0xffff_ffe0,
-            self.hcc_params2
-        )
-    }
-}
-
-#[repr(C)]
-struct HscParam1 {
-    data: u32,
-}
-
-impl HscParam1 {
-    pub fn max_device_slots(&self) -> u8 {
-        (self.data & 0xff) as u8
-    }
-
-    pub fn max_ports(&self) -> u8 {
-        (self.data >> 24 & 0xff) as u8
-    }
-}
-
-impl core::fmt::Display for HscParam1 {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(
-            f,
-            "0x{:08x} (slots: {} ports: {})",
-            self.data,
-            self.max_device_slots(),
-            self.max_ports()
-        )
-    }
-}
-
-#[repr(C)]
-struct HscParam2 {
-    data: u32,
-}
-
-impl HscParam2 {
-    pub fn max_scratchpad_buf(&self) -> usize {
-        let hi = (self.data >> 21 & 0b11111) as usize;
-        let lo = (self.data >> 27 & 0b11111) as usize;
-        (hi << 5) | lo
-    }
-}
-
-impl core::fmt::Display for HscParam2 {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(
-            f,
-            "0x{:08x} (max_scratchpad_buf: {})",
-            self.data,
-            self.max_scratchpad_buf()
-        )
-    }
-}
-
-#[repr(C)]
-struct HccParams1 {
-    data: u32,
-}
-
-impl HccParams1 {
-    pub fn xecp(&self) -> u16 {
-        (self.data >> 16) as u16
-    }
-}
-
-impl core::fmt::Display for HccParams1 {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "0x{:08x} (xECP: 0x{:08x})", self.data, self.xecp())
-    }
-}
-
-#[repr(C, packed(4))]
-#[derive(Debug)]
-pub struct OperationalRegisters {
-    usbcmd: u32,
-    usbsts: u32,
-    pagesize: u32,
-    _rsvd_1: [u32; 2],
-    dnctrl: u32,
-    crcr: u64,
-    _rsvd_2: [u32; 4],
-    dcbaap: u64,
-    config: u32,
-}
-
-pub struct Doorbell {
-    data: u32,
-}
-
-impl Doorbell {
-    pub fn set_db_target(&mut self, target: u8) {
-        self.data = self.data & 0xffff_fff0 | target as u32;
-    }
-
-    pub fn set_db_stream_id(&mut self, stream_id: u16) {
-        self.data = self.data & 0x0000_ffff | (stream_id as u32) << 16;
-    }
-}
+mod registers;
+use registers::*;
 
 pub struct Controller<'a> {
     cap_regs: &'a mut CapabilityRegisters,
@@ -142,7 +17,6 @@ impl<'a> Controller<'a> {
         debug!("cap regs: {}", cap_regs);
         let op_regs =
             &mut *((mmio_base + cap_regs.cap_length as usize) as *mut OperationalRegisters);
-        debug!("op_regs: {:?}", op_regs);
         let doorbell_first =
             (mmio_base + (cap_regs.db_off & 0xffff_fffc) as usize) as *mut Doorbell;
         Controller {
---
+++ kernel/src/usb/registers.rs
@@ -0,0 +1,126 @@
+#[repr(C, packed(4))]
+pub struct CapabilityRegisters {
+    pub cap_length: u8,
+    reserved: u8,
+    pub hci_version: u16,
+    pub hcs_params1: HscParam1,
+    pub hcs_params2: HscParam2,
+    pub hcs_params3: u32,
+    pub hcc_params1: HccParams1,
+    pub db_off: u32,
+    pub rts_off: u32,
+    pub hcc_params2: u32,
+}
+
+impl core::fmt::Display for CapabilityRegisters {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(
+            f,
+            "cap_length: {}, hci_version: 0x{:02x}, hcs_params1: {}, hcs_params2: {}, hcs_params3: 0x{:08x}, hcc_params1: {}, db_off: 0x{:08x}, rts_off: 0x{:08x}, hcc_params2: 0x{:08x}",
+            self.cap_length,
+            self.hci_version,
+            self.hcs_params1,
+            self.hcs_params2,
+            self.hcs_params3,
+            self.hcc_params1,
+            self.db_off & 0xffff_fffc,
+            self.rts_off & 0xffff_ffe0,
+            self.hcc_params2
+        )
+    }
+}
+
+#[repr(C)]
+pub struct HscParam1 {
+    data: u32,
+}
+
+impl HscParam1 {
+    pub fn max_device_slots(&self) -> u8 {
+        (self.data & 0xff) as u8
+    }
+
+    pub fn max_ports(&self) -> u8 {
+        (self.data >> 24 & 0xff) as u8
+    }
+}
+
+impl core::fmt::Display for HscParam1 {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(
+            f,
+            "0x{:08x} (slots: {} ports: {})",
+            self.data,
+            self.max_device_slots(),
+            self.max_ports()
+        )
+    }
+}
+
+#[repr(C)]
+pub struct HscParam2 {
+    data: u32,
+}
+
+impl HscParam2 {
+    pub fn max_scratchpad_buf(&self) -> usize {
+        let hi = (self.data >> 21 & 0b11111) as usize;
+        let lo = (self.data >> 27 & 0b11111) as usize;
+        (hi << 5) | lo
+    }
+}
+
+impl core::fmt::Display for HscParam2 {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(
+            f,
+            "0x{:08x} (max_scratchpad_buf: {})",
+            self.data,
+            self.max_scratchpad_buf()
+        )
+    }
+}
+
+#[repr(C)]
+pub struct HccParams1 {
+    data: u32,
+}
+
+impl HccParams1 {
+    pub fn xecp(&self) -> u16 {
+        (self.data >> 16) as u16
+    }
+}
+
+impl core::fmt::Display for HccParams1 {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "0x{:08x} (xECP: 0x{:08x})", self.data, self.xecp())
+    }
+}
+
+#[repr(C, packed(4))]
+pub struct OperationalRegisters {
+    pub usbcmd: u32,
+    pub usbsts: u32,
+    pub pagesize: u32,
+    pub _rsvd_1: [u32; 2],
+    pub dnctrl: u32,
+    pub crcr: u64,
+    pub _rsvd_2: [u32; 4],
+    pub dcbaap: u64,
+    pub config: u32,
+}
+
+pub struct Doorbell {
+    data: u32,
+}
+
+impl Doorbell {
+    pub fn set_db_target(&mut self, target: u8) {
+        self.data = self.data & 0xffff_fff0 | target as u32;
+    }
+
+    pub fn set_db_stream_id(&mut self, stream_id: u16) {
+        self.data = self.data & 0x0000_ffff | (stream_id as u32) << 16;
+    }
+}
