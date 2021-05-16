219c12c97cbeb185dbcd4a473072567e3e8f1020
Satoshi Kojima <skoji@skoji.jp>
2021-05-16 21:40:48 +0900

    update bitwise macro

--- kernel/src/bitwise_macro.rs
+++ kernel/src/bitwise_macro.rs
@@ -1,8 +1,8 @@
 #[macro_export]
 macro_rules! bit_setter {
-    ($field:tt : $field_type:ty; $vis:vis $name:ident) => {
-        $vis fn $name(&mut self, bit: usize, value: bool) {
-            let b: $field_type = 1 << bit;
+    ($field:tt : $field_type:ty; $bit:literal, $vis:vis $name:ident) => {
+        $vis fn $name(&mut self, value: bool) {
+            let b: $field_type = 1 << $bit;
             if (value) {
                 self.$field |= b;
             } else {
@@ -14,9 +14,9 @@ macro_rules! bit_setter {
 
 #[macro_export]
 macro_rules! bit_getter {
-    ($field:tt : $field_type:ty; $vis:vis $name:ident) => {
-        $vis fn $name(&mut self, bit: usize) -> bool {
-            let b: $field_type = 1 << bit;
+    ($field:tt : $field_type:ty; $bit:literal, $vis:vis $name:ident) => {
+        $vis fn $name(&mut self) -> bool {
+            let b: $field_type = 1 << $bit;
             (self.$field & b) == b
         }
     }
@@ -30,22 +30,24 @@ mod test {
     }
 
     impl Tester {
-        bit_setter!(data: u32; pub set_data);
-        bit_setter!(data2: u8; pub set_data2);
-        bit_getter!(data: u32; pub get_data);
-        bit_getter!(data2: u8; pub get_data2);
+        bit_setter!(data: u32; 4, pub set_data_4);
+        bit_setter!(data2: u8; 5, pub set_data2_5);
+        bit_getter!(data: u32; 7, pub get_data_7);
+        bit_getter!(data: u32; 11,  pub get_data_11);
+        bit_getter!(data2: u8; 7, pub get_data2_7);
+        bit_getter!(data2: u8; 3, pub get_data2_3);
     }
 
     #[test_case]
     fn test_u32_set() {
         let mut t = Tester { data: 0, data2: 0 };
-        t.set_data(4, true);
+        t.set_data_4(true);
         assert_eq!(t.data, 0b10000);
     }
     #[test_case]
     fn test_u8_set() {
         let mut t = Tester { data: 0, data2: 0 };
-        t.set_data2(5, true);
+        t.set_data2_5(true);
         assert_eq!(t.data2, 0b100000);
     }
     #[test_case]
@@ -54,8 +56,8 @@ mod test {
             data: 0b1111_0101_1011_1000_0001_0100_1100_0111,
             data2: 0b1010_0111,
         };
-        assert!(t.get_data(7));
-        assert!(!t.get_data(11));
+        assert!(t.get_data_7());
+        assert!(!t.get_data_11());
     }
     #[test_case]
     fn test_u8_get() {
@@ -63,7 +65,7 @@ mod test {
             data: 0b1111_0101_1011_1000_0001_0100_1100_0111,
             data2: 0b1010_0111,
         };
-        assert!(t.get_data2(7));
-        assert!(!t.get_data2(3));
+        assert!(t.get_data2_7());
+        assert!(!t.get_data2_3());
     }
 }
