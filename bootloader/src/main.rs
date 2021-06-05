9a13092d0383f52e2b9c312e3f18732b64396187
Satoshi Kojima <skoji@skoji.jp>
2021-06-05 18:23:48 +0900

    align Memory map buffer

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -8,7 +8,10 @@ extern crate alloc;
 use alloc::string::ToString;
 use console::gop;
 use core::arch::asm;
-use core::fmt::Write;
+use core::{
+    fmt::Write,
+    mem::{size_of, MaybeUninit},
+};
 use elf_rs::*;
 use proto::console;
 use uefi::{
@@ -22,7 +25,7 @@ use uefi::{
 };
 
 static mut LOGGER: Option<uefi::logger::Logger> = None;
-static mut MMAP_BUF: [u8; 4096 * 4] = [0; 4096 * 4];
+static mut MMAP_BUF: [MaybeUninit<MemoryDescriptor>; 1024] = [MaybeUninit::uninit(); 1024];
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
@@ -34,7 +37,7 @@ struct FrameBufferInfo {
 #[allow(dead_code)]
 fn set_gop_mode(gop: &mut GraphicsOutput) {
     let mut mode: Option<gop::Mode> = None;
-    for m in gop.modes().into_iter() {
+    for m in gop.modes() {
         let m = m.unwrap();
         let res = m.info().resolution();
 
@@ -88,7 +91,6 @@ fn efi_main(handle: Handle, st: SystemTable<Boot>) -> Status {
         // set gop mode if it is not in QEMU
         set_gop_mode(gop);
     }
-    log::error!("intentional panic");
     writeln!(stdout, "Hello from rust").unwrap();
     writeln!(stdout, "Firmware Vendor {}", st.firmware_vendor()).unwrap();
 
@@ -108,7 +110,13 @@ fn efi_main(handle: Handle, st: SystemTable<Boot>) -> Status {
         .unwrap();
     let memmap_file = memmap_file.into_type().unwrap().unwrap();
     if let Regular(mut memmap_file) = memmap_file {
-        let (_, memmap_iter) = unsafe { bt.memory_map(&mut MMAP_BUF).unwrap().unwrap() };
+        let buf: &mut [u8] = unsafe {
+            core::slice::from_raw_parts_mut(
+                MMAP_BUF.as_mut_ptr() as *mut u8,
+                MMAP_BUF.len() * size_of::<MemoryDescriptor>(),
+            )
+        };
+        let (_, memmap_iter) = bt.memory_map(buf).unwrap().unwrap();
         memmap_file
             .write("Index, Type, PhysicalStart, NumberOfPages, Attribute\n".as_bytes())
             .unwrap()
@@ -150,7 +158,7 @@ fn efi_main(handle: Handle, st: SystemTable<Boot>) -> Status {
     kernel_file.read(kernel_file_buf).unwrap().unwrap();
     kernel_file.close();
 
-    let elf = match Elf::from_bytes(&kernel_file_buf).unwrap() {
+    let elf = match Elf::from_bytes(kernel_file_buf).unwrap() {
         Elf::Elf64(e) => e,
         Elf::Elf32(_) => {
             panic!("Elf32 is not supported");
